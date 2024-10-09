b09776d9d32924f981ab6f0ffb3a483dfa4ac45c
rami-mohamad <rami_zezo1986@hotmail.com>
2024-10-09 11:43:06 +0200

    added live coding for for-loops

--- 03-PB/08-Schleifen-09-10-24/live-coding/for-loops.js
+++ 03-PB/08-Schleifen-09-10-24/live-coding/for-loops.js
@@ -28,6 +28,76 @@ for (let count = 0; count <= 5; count++) {
   console.log(count);
 }
 
-for (let i = 100; i <= 110; i++) {
+for (let i = 100; i <= 105; i++) {
   console.log(i);
 }
+
+console.log("-------");
+
+// 10, 12, 14,16, 18, 20
+
+// for (let i = 10; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+// i = i+ 2 === i += 2
+for (let i = 10; i <= 20; i += 2) {
+  console.log(i);
+}
+
+console.log("-------");
+// 5,4,3,2,1,0
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+
+console.log("-------");
+
+let sum = 0;
+
+for (let i = 0; i <= 5; i++) {
+  console.log(`Adding ${i} to ${sum}`);
+
+  sum += i;
+}
+console.log("Final Sum:", sum);
+
+console.log("-------");
+
+// ....*....*....*....*....* // 50 Char
+
+let string = "";
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 5 === 0) {
+//     string += "*";
+//   } else {
+//     string += ".";
+//   }
+// }
+// console.log(string);
+
+for (let i = 0; i < 10; i++) {
+  string += "....*";
+}
+console.log(string);
+
+console.log("-------");
+
+const autos = ["Audi", "Bmw", "Benz", "Opel"];
+
+// console.log(autos[0]);
+// console.log(autos[1]);
+
+for (let i = 0; i < autos.length; i++) {
+  console.log(autos[i]);
+}
+
+const autosLowerCase = [];
+console.log(autosLowerCase);
+
+for (let i = 0; i < autos.length; i++) {
+  autosLowerCase.push(autos[i].toLowerCase());
+}
+console.log(autosLowerCase);
