29e4d900f5df0f8c07c3ce4f62a6b2f10547c991
rami-mohamad <rami_zezo1986@hotmail.com>
2024-10-15 12:02:47 +0200

    added task in live coding

--- 03-PB/11-wiederholung-tag-loops-methods-15-10-24/live-coding/loops-wiederholung.js
+++ 03-PB/11-wiederholung-tag-loops-methods-15-10-24/live-coding/loops-wiederholung.js
@@ -21,8 +21,8 @@ import rls from "readline-sync";
 // }
 // console.log(max);
 
-let count = 2;
-let max = 0;
+// let count = 2;
+// let max = 0;
 
 // do {
 //   let number = rls.questionInt("Gib eine Zahl ein: ");
@@ -34,16 +34,71 @@ let max = 0;
 // console.log(max);
 
 // -----------------
-while (count < 5) {
-  console.log("Welcome to the Big number app");
+// while (count < 5) {
+//   console.log("Welcome to the Big number app");
 
-  let number = rls.questionInt("Gib eine Zahl ein: ");
-  if (number > max) {
-    max = number;
-  }
-  count++;
-}
-console.log(max);
+//   let number = rls.questionInt("Gib eine Zahl ein: ");
+//   if (number > max) {
+//     max = number;
+//   }
+//   count++;
+// }
+// console.log(max);
 
 // aufgabe
 //Frage den Benutzer nach 5 Zahlen, speichere sie in einem Array, und berechne die Summe der Zahlen mit einer for-Schleife oder while-schleife und array zeigen.
+
+// let numbers = []; // [1,2,3,4,5]
+// let sum = 0;
+
+// for (let i = 0; i < 5; i++) {
+//   let newNum = parseInt(rls.question(`Gib eine Zahl ${i + 1} ein:`));
+//   numbers.push(newNum);
+// }
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(`Die Summe meinem Array ${numbers} ist : ${sum}`);
+
+// Frage den Benutzer nach 5 Wörtern und gib das Array in umgekehrter Reihenfolge mit einer for-Schleife aus.
+
+let words = [];
+// for (let i = 0; i < 5; i++) {
+//   words.push(rls.question("Gib ein Wort ein :"));
+// }
+
+// for (let i = words.length - 1; i >= 0; i--) {
+//   console.log(words[i]);
+// }
+// let i = 0;
+// while (i < 5) {
+//   words.push(rls.question("Gib ein Wort ein :"));
+//   i++;
+// }
+// console.log(words.length); // 4
+// console.log(i); // 5
+// while (words.length > 0) {
+//   console.log(words.pop()); //
+// }
+// console.log(words);
+
+// Erstelle ein verschachteltes Array mit mindestens 3 Zeilen und 3 Spalten und finde die kleinste Zahl mit einer verschachtelten for-schleife.
+
+let nestedArray = [
+  [3, 5, 7, -1000],
+  [2, 8, 6],
+  [1, 9, 4],
+  [0, -100, -500],
+];
+let min = nestedArray[0][0];
+let sum = 0;
+for (let i = 0; i < nestedArray.length; i++) {
+  for (let j = 0; j < nestedArray[i].length; j++) {
+    if (nestedArray[i][j] < min) {
+      min = nestedArray[i][j];
+    }
+    sum += nestedArray[i][j];
+  }
+}
+console.log(min);
+console.log(sum);
