f206f7be04892791c14440aa70d3e43c4c27b867
rami-mohamad <rami_zezo1986@hotmail.com>
2024-11-11 14:17:38 +0100

    updated live coding

--- 03-PB/24-Klassen-OOP-11-11-24/live-coding/bankKonto.js
+++ 03-PB/24-Klassen-OOP-11-11-24/live-coding/bankKonto.js
@@ -24,9 +24,31 @@ class BankAccount {
   deposit(balance) {
     this.#balance += balance;
   }
+
+  // Method geld abheben mit Validierung
+
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      console.log("Nicht genüg Geld!");
+    } else {
+      this.#balance -= amount;
+      console.log(`Your new balance is ${this.#balance}`);
+    }
+  }
+
+  // Method komplett Account info zu bekommen
+
+  displayAcoountInfo() {
+    console.log(`Account number : ${this.#iban}
+        Account holder : ${this.#inhaber}
+        Balance: ${this.#balance}`);
+  }
 }
 
 const myAccount = new BankAccount("1234454353", "John", 1000);
 
 myAccount.deposit(1000);
 console.log(myAccount.balance);
+myAccount.withdraw(1900);
+
+myAccount.displayAcoountInfo();
